cd81292911cef842d036f6d1f6399e93befbad37
SaifAliRko <saif.ali96597@gmail.com>
2020-11-10 14:23:16 +0100

    making the base layout of website

---
+++ mywebsite/app.py
@@ -0,0 +1,6 @@
+from puppycompanyblog import app
+
+
+if __name__ == '__main__':
+    app.run(debug= True)
+    
---
+++ mywebsite/puppycompanyblog/core/views.py
@@ -0,0 +1,15 @@
+# under core / views.py
+
+
+from flask import render_template, request, Blueprint
+
+core = Blueprint('core', __name__)
+
+@core.route('/')
+def index():
+    # more to come
+    return render_template('index.html')
+
+@core.route('/info')
+def info():
+    return render_template('info.html')
