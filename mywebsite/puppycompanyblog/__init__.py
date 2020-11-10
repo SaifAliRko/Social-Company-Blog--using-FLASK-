d28fe4581a9cdd4388a799288c361ecd55b867a5
SaifAliRko <saif.ali96597@gmail.com>
2020-11-10 15:52:49 +0100

    adding templates

--- mywebsite/puppycompanyblog/__init__.py
+++ mywebsite/puppycompanyblog/__init__.py
@@ -35,8 +35,11 @@ login_manager.login_view = 'users.login'
 
 from puppycompanyblog.core.views import core
 from puppycompanyblog.users.views import users
+from puppycompanyblog.blog_posts.views import blog_posts
 from puppycompanyblog.error_pages.handlers import error_pages
+
 
 app.register_blueprint(core)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
 app.register_blueprint(error_pages)
--- mywebsite/puppycompanyblog/core/views.py
+++ mywebsite/puppycompanyblog/core/views.py
@@ -1,5 +1,5 @@
 # under core / views.py
-
+from puppycompanyblog.models import BlogPost
 
 from flask import render_template, request, Blueprint
 
@@ -8,7 +8,10 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     # more to come
-    return render_template('index.html')
+    page = request.args.get('page',1,type = int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page , per_page =5)
+
+    return render_template('index.html', blog_posts=blog_posts)
 
 @core.route('/info')
 def info():
