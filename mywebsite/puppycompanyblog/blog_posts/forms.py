d8db5f2cf64d305329024d968cd459407fa4f39d
SaifAliRko <saif.ali96597@gmail.com>
2020-11-16 10:36:04 +0100

    Styling the final project

--- mywebsite/puppycompanyblog/blog_posts/forms.py
+++ mywebsite/puppycompanyblog/blog_posts/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
+
 class BlogPostForm(FlaskForm):
-    title = StringField('Title',validators=[DataRequired()])
-    text = TextAreaField('Text',validators=[DataRequired()])
-    submit = SubmitField("Post")
+    # no empty titles or text possible
+    # we'll grab the date automatically from the Model later
+    title = StringField('Title', validators=[DataRequired()])
+    text = TextAreaField('Text', validators=[DataRequired()])
+    submit = SubmitField('BlogPost')
